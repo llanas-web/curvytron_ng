b95142cb73055eb5adc27b3e62135779baf67606
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2023-08-14 14:02:03 +0200

    Revert " feat: Separating server and front code sources"
    This reverts commit 5996ae0b98c5652e42fee2d658eb469b68b04b20.

---
+++ server/service/Chat.ts
@@ -0,0 +1,31 @@
+import { Collection } from '@shared/collection';
+import { BaseChat } from '@shared/service/BaseChat';
+
+import { Message } from '../models/Message';
+import { FloodFilter } from './FloodFilter';
+
+/**
+ * Chat system
+ */
+export class Chat extends BaseChat {
+
+    floodFilter: any;
+    
+    /** OVERRIDE */
+    declare messages: Collection<Message>;
+
+    constructor() {
+
+        super();
+
+        this.floodFilter = new FloodFilter(this.messages);
+    }
+
+    /**
+     * Is message valid?
+     */
+    isValid(message: Message): boolean {
+        const length = message.content.length;
+        return length > 0 && length <= Message.maxLength && this.floodFilter.isValid(message);
+    }
+}
